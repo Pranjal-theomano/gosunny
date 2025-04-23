60bc35b7b12077e56d07f2ba63c130771f87ec6f
Pranjal jain <88794240+Pranjaljain77@users.noreply.github.com>
2025-04-23 17:26:24 +0530

    Fix chatbot Responsive

--- src/components/ChatBox/ChatBox.js
+++ src/components/ChatBox/ChatBox.js
@@ -237,7 +237,7 @@ const ChatBox = ({ exampleQuestions = [] }) => {
                 id="chat-user-input"
                 rows={1}
                 className={styles["chat-input-box"] + " w-100"}
-                style={{ width: '320px' }}
+                style={{ width: '300px' }}
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
                 placeholder="Ask anything about solar and this process"
